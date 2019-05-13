bc2c6194e2b3c7cc30aef9fec0c060ce76ce6f06
Elias Rosales <eliasrosales88@gmail.com>
2019-05-14 01:16:29 +0200

    Incorporing geolocation code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit{
+
+  longitude;
+  latitude;
+  constructor(private geolocation: Geolocation) {}
+
+  ngOnInit(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.longitude);
+      console.log(resp.coords.latitude);
+      
+    this.longitude = resp.coords.longitude;       
+    this.latitude = resp.coords.latitude;       
+      
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    
+      let watch = this.geolocation.watchPosition();
+      watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+      });
+  }
+  
+
+}
